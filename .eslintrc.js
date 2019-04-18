334caa5fc9f5546f0f8b89d591078679e582df82
Phojie <Phojie>
2019-04-18 08:42:10 +0800

    test

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+    'prettier/vue',
+
+    "plugin:vue/recommended",
+    "eslint:recommended",
+  ],
+  plugins: [
+    'prettier'
+  ],
+  // add your custom rules here
+  rules: {
+    "vue/component-name-in-template-casing": ["error", "PascalCase"],
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+  },
+  globals: {
+    $nuxt: true
+  },
+  parserOptions: {
+    parser: "babel-eslint"
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <v-card class="green" style="width:200px;  height:200px">
+      test
+      <v-btn>green</v-btn>
+    </v-card>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      test: 'test'
+    }
+  }
+}
+</script>
